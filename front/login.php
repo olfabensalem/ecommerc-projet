d513838a7b75956a12fc7a859542dbe60b88bf66
olfa bensalem <olfabensalem568@gmail.com>
2020-06-15 17:14:54 +0200

    msj

--- front/login.php
+++ front/login.php
@@ -109,7 +109,7 @@
                         <h2>Login</h2>
                         <form action="login.php" method="post">
                             <div class="group-input">
-                                <label for="username">Username  *</label>
+                                <label for="username">Username or email *</label>
                                 <input type="text" id="username" name="username" autocomplete="off">
                                 <?php
                                     if(isset($erUsername)){
