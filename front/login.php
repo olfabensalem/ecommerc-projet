6b42eab543f1ba9f7c37ed26325f737bd5c3af21
olfa bensalem <olfabensalem568@gmail.com>
2020-07-17 22:42:03 +0200

    msj

--- front/login.php
+++ front/login.php
@@ -109,7 +109,7 @@
                         <h2>Login</h2>
                         <form action="login.php" method="post">
                             <div class="group-input">
-                                <label for="username">Usernames or adress  *</label>
+                                <label for="username">Usernames or mail adress  *</label>
                                 <input type="text" id="username" name="username" autocomplete="off">
                                 <?php
                                     if(isset($erUsername)){
@@ -118,7 +118,7 @@
                                 ?>
                             </div>
                             <div class="group-input">
-                                <label for="password">Passwords  *</label>
+                                <label for="password">Passwords or mot de passe *</label>
                                 <input type="password" id="password" name="password" autocomplete="off">
                                 <?php
                                     if(isset($erPassword)){
